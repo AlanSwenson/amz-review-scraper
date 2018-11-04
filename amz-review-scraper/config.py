e6cf406cac8f32d8103ab37c6dc5a4eb655fb101
Alan Swenson <aswens0276@gmail.com>
2018-11-03 22:58:07 -0400

    Update config.py
    @davidbegin line 19 is where i am pulling in my .env dict and it becomes a string

--- amz-review-scraper/config.py
+++ amz-review-scraper/config.py
@@ -13,6 +13,8 @@ POSTGRES_USER = get_env_variable("POSTGRES_USER")
 POSTGRES_PW = get_env_variable("POSTGRES_PW")
 POSTGRES_DB = get_env_variable("POSTGRES_DB")
 
+
+# This is pulling in a dict and making it a string
 # proxies
 proxies = get_env_variable("proxies")
 
