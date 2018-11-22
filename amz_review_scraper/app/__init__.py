0b9651848859b1a28f1229c152368d0812c21fb4
Alan Swenson <aswens0276@gmail.com>
2018-11-22 15:58:29 -0500

    solved issue of pyscopg2 warning

--- amz_review_scraper/app/__init__.py
+++ amz_review_scraper/app/__init__.py
@@ -11,6 +11,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     migrate = Migrate(app, db)
     with app.app_context():
         db.init_app(app)
