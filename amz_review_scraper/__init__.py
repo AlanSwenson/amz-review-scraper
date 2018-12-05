f0bce9e8ef6547424edda459b203448018b6053d
Alan Swenson <aswens0276@gmail.com>
2018-12-04 19:59:39 -0500

    adding csrf updates

--- amz_review_scraper/__init__.py
+++ amz_review_scraper/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_s3 import FlaskS3
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 from amz_review_scraper.config import Config
 
@@ -10,6 +12,10 @@ db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
 s3 = FlaskS3()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 
 
 def create_app(config_class=Config):
@@ -23,6 +29,8 @@ def create_app(config_class=Config):
         migrate.init_app(app, db)
         csrf.init_app(app)
         s3.init_app(app)
+        bcrypt.init_app(app)
+        login_manager.init_app(app)
         register_blueprints(app)
 
     @app.route("/", methods=["POST", "GET"])
