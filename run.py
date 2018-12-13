42dea5633a3e1f770f581675eb90fe75c77c987a
Alan Swenson <aswens0276@gmail.com>
2018-12-12 19:58:36 -0500

    removed more code from run.py

--- run.py
+++ run.py
@@ -1,11 +1,5 @@
-import amz_review_scraper.amzscraper as amazon
-import amz_review_scraper.urls as urls
-import amz_review_scraper.asin_validation as asin_validation
-import amz_review_scraper.get_soup as get_soup
-
 from amz_review_scraper import create_app, db
 
 
 if __name__ == "__main__":
-    # main()
     app.run(debug=False)
