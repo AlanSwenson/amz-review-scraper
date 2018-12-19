1db78d1e85b5dce3113786f64828e4403ab2c0c8
Alan Swenson <aswens0276@gmail.com>
2018-12-18 19:45:54 -0500

    add lowecase cleansing for email address in signup and login

--- amz_review_scraper/signup/views.py
+++ amz_review_scraper/signup/views.py
@@ -24,7 +24,7 @@ def index():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
             "utf-8"
         )
-        user = User(email=form.email.data, password=hashed_password)
+        user = User(email=form.email.data.lower(), password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash("Your account has been created! You are now able to log in", "success")
