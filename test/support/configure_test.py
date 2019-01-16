3e0173544b51daaa302b9beeac210eeb7f605b64
Alan Swenson <aswens0276@gmail.com>
2019-01-15 21:17:38 -0500

    create pytest fixture for testing DB

--- test/support/configure_test.py
+++ test/support/configure_test.py
@@ -1,10 +1,32 @@
 import pytest
 
-from amz_review_scraper import create_app
+from amz_review_scraper import create_app, db
+from amz_review_scraper.config import get_env_db_url
 
 
-@pytest.fixture
+@pytest.yield_fixture
 def app():
-    app = create_app()
-    # app.config.from_object("amz_review_scraper.config.TestingConfig")
-    return app
+    def _app(config_class):
+        app = create_app(config_class)
+        app.app_context().push()
+        # with app.app_context():
+        DB_URL = get_env_db_url("testing")
+
+        if app.config["SQLALCHEMY_DATABASE_URI"] == DB_URL:
+            # app.config.from_object("amz_review_scraper.config.TestingConfig")
+            from amz_review_scraper.models.users_items_association import (
+                users_items_association,
+            )
+            from amz_review_scraper.models.user import User
+            from amz_review_scraper.models.item import Item
+            from amz_review_scraper.models.review import Review
+
+            db.create_all()
+            return app
+        else:
+            return app
+            db.session.remove()
+
+    yield _app
+    db.session.remove()
+    db.drop_all()
--- test/test_config.py
+++ test/test_config.py
@@ -1,24 +1,31 @@
 import pytest
 
 from test.support.configure_test import app
-from amz_review_scraper.config import get_env_db_url
+from amz_review_scraper.config import (
+    get_env_db_url,
+    TestingConfig,
+    DevelopmentConfig,
+    ProductionConfig,
+)
+from amz_review_scraper import db
+import amz_review_scraper.models.item as item
+from amz_review_scraper.models.user import User
+import amz_review_scraper.model_functions as model_functions
 
 
 def test_development_config(app):
-    app.config.from_object("amz_review_scraper.config.DevelopmentConfig")
-
+    app = app(DevelopmentConfig)
     DB_URL = get_env_db_url("development")
-
+    print(DB_URL)
     assert app.config["DEBUG"]
     assert not app.config["TESTING"]
     assert app.config["SQLALCHEMY_DATABASE_URI"] == DB_URL
 
 
 def test_testing_config(app):
-    app.config.from_object("amz_review_scraper.config.TestingConfig")
-
+    app = app(TestingConfig)
     DB_URL = get_env_db_url("testing")
-
+    print(DB_URL)
     assert app.config["DEBUG"]
     assert app.config["TESTING"]
     assert not app.config["PRESERVE_CONTEXT_ON_EXCEPTION"]
@@ -26,10 +33,21 @@ def test_testing_config(app):
 
 
 def test_production_config(app):
-    app.config.from_object("amz_review_scraper.config.ProductionConfig")
-
+    app = app(ProductionConfig)
     DB_URL = get_env_db_url("production")
-
+    print(DB_URL)
     assert not app.config["DEBUG"]
     assert not app.config["TESTING"]
     assert app.config["SQLALCHEMY_DATABASE_URI"] == DB_URL
+
+
+def test_db_create(app):
+    app = app(TestingConfig)
+
+    scraped_item = item.Item(
+        name="test name", customer_reviews_count="99", asin="1111111111"
+    )
+    scraped_item = item.save_or_update(scraped_item)
+    model_functions.save_to_db()
+
+    assert db.session.query(item.Item).one()
