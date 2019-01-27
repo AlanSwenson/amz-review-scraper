4d1d5c3b84edb27920912013ed810b409894f7e3
Alan Swenson <aswens0276@gmail.com>
2019-01-26 21:29:48 -0500

    add refresh button and last_scrapedto /results

--- amz_review_scraper/results/views.py
+++ amz_review_scraper/results/views.py
@@ -2,6 +2,9 @@ from flask import redirect, render_template, url_for, Blueprint, flash
 from flask_login import current_user
 
 import amz_review_scraper.models.item as item
+import amz_review_scraper.urls as urls
+import amz_review_scraper.get_soup as get_soup
+import amz_review_scraper.amzscraper as amazon
 
 results_blueprint = Blueprint(
     "results",
@@ -10,6 +13,21 @@ results_blueprint = Blueprint(
     static_folder="./static",
     template_folder="templates",
 )
+
+
+@results_blueprint.route("/refresh_asin/<asin>", methods=["POST", "GET"])
+def refresh_asin(asin):
+    url = urls.create_url(asin)
+    soup = get_soup.boil_soup(url, asin)
+    if soup.status_code != None:
+        flash(
+            "ASIN returned Status Code: "
+            + str(soup.status_code)
+            + " Please check your ASIN and try Again"
+        )
+    else:
+        amazon.scrape(soup, asin)
+    return redirect(url_for("results.index"))
 
 
 @results_blueprint.route("/")
