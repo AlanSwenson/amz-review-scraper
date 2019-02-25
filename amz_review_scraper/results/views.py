01ec9124a546f771515a3b837794dc7aeaeea7d7
Alan Swenson <aswens0276@gmail.com>
2019-02-25 14:26:57 -0500

    add feature to remove tracking of ASIN for a user

--- amz_review_scraper/results/views.py
+++ amz_review_scraper/results/views.py
@@ -5,6 +5,10 @@ import amz_review_scraper.models.item as item
 import amz_review_scraper.urls as urls
 import amz_review_scraper.get_soup as get_soup
 import amz_review_scraper.amzscraper as amazon
+from amz_review_scraper import db
+from amz_review_scraper.models.users_items_association import users_items_association
+from amz_review_scraper.models.item import Item
+from amz_review_scraper.models.user import User, get_current_user
 
 
 results_blueprint = Blueprint(
@@ -31,6 +35,26 @@ def refresh_asin(asin):
         else:
             amazon.scrape(soup, asin)
         return redirect(url_for("results.index"))
+
+
+@results_blueprint.route("/stop_tracking/<asin>", methods=["POST", "GET"])
+@jwt_required
+def stop_tracking(asin):
+    if get_jwt_identity() is not None:
+        link_to_delete = (
+            Item.query.join(
+                users_items_association,
+                (users_items_association.c.user_id == get_jwt_identity()),
+            )
+            .filter(users_items_association.c.item_id == Item.id)
+            .filter(Item.asin == asin)
+            .first()
+        )
+        user = get_current_user()
+        link_to_delete.users.remove(user)
+        db.session.commit()
+
+    return redirect(url_for("results.index"))
 
 
 @results_blueprint.route("/")
